1679afcbba4bf1ea5ac058fb72e6201ced967e38
Roman Apostol <roman@mate.academy>
2019-01-30 14:38:40 +0200

    Create script3.js

---
+++ training2/script3.js
@@ -0,0 +1,11 @@
+function basic_op(operator, value1, value2) {
+  if (operator == '+') {
+    return value1 + value2;
+  } else if (operator == '-') {
+    return value1 - value2;
+  } else if (operator == '*') {
+    return value1 * value2;
+  } else if (operator == '/') {
+    return value1 / value2;
+  }
+}
