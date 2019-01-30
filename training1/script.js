a360320bf536bd356d119018c94e02e9b455f522
Roman Apostol <roman@mate.academy>
2019-01-30 13:37:18 +0200

    Update script.js

--- training1/script.js
+++ training1/script.js
@@ -1,20 +1,10 @@
-'use strict';
-
-function rentalCarCost(d) {
-  let carRent = 40;
-
-  if (d >= 7) {
-    return d * carRent - 50;
-  } else if (d >= 3) {
-    return d * carRent - 20;
-  } else if (d <= 0) {
-    return 0;
-  } else {
-    return d * carRent;
-  }
+var d = prompt('Введіть кількість днів', '');
+var sum;
+if (d >= 7) {
+  sum = d * 40 - 50;
+} else if (d >= 3) {
+  sum = d * 40 - 20;
+} else {
+  sum = d * 40;
 }
-
-console.log(rentalCarCost(-8));
-console.log(rentalCarCost(2));
-console.log(rentalCarCost(5));
-console.log(rentalCarCost(10));
+console.log(sum);
